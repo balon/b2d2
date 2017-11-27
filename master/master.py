e277ff824aa69e22c9dc064a38f804f02df0814a
TJ Balon <balon@tjbalon.io>
2017-11-26 23:39:23 -0500

    Re-Order of Handling

---
+++ master/master.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+'''
+	File name: master.py
+	Purpose: Server binary for b2bkup
+	Authors: TJ Balon, Matt Topor
+	Date Modified: 11/26/2017
+	Python Version: 3.5
+'''
+# This code is property of TangoNetworksLLC & Affiliates... 
+# All code is subject to the terms and conditioned defined in
+# 'LICENSE.txt', which is part of this source code package.
---
+++ minion/client.py
@@ -0,0 +1,27 @@
+'''
+	File name: client.py
+	Purpose: Client Class for b2bkup
+	Authors: TJ Balon, Matt Topor
+	Date Modified: 11/26/2017
+	Python Version: 3.5
+'''
+# This code is property of TangoNetworksLLC & Affiliates... 
+# All code is subject to the terms and conditioned defined in
+# 'LICENSE.txt', which is part of this source code package.
+import socket
+
+class ClientHandler:
+	def __init__(self, ip, port):
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print(ip)
+		print(port)
+		self.client.connect((ip, port))
+
+	def read(self):
+		return self.client.recv(1024)
+
+	def sendall(self, buff):
+		self.client.sendall(buff)
+
+	def close(self):
+		self.client.close()
